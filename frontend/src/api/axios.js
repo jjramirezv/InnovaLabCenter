46fa215a6ab1f88ad1c4a1fd0544ba2450895b47
jjramirezv <e_2024100594i@uncp.edu.pe>
2026-01-14 15:18:05 -0500

    Conectando al backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api', // La URL de tu backend
+    baseURL: 'https://innovalabcenter-production.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
